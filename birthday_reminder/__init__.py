e6643d9ca9506951cca5c1163eaa87ff1a501a1b
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-12-26 23:19:14 +0800

    birthday_reminder: bare implementation of start command

--- birthday_reminder/__init__.py
+++ birthday_reminder/__init__.py
@@ -2,4 +2,5 @@ import os
 
 import birthday_reminder.logging  # type: ignore (not meant to be accessed, stuffs are done in the file)
 
+BOT_TOKEN: str = os.getenv("ELYSIAN_BDAY_BOT_TOKEN", "")
 ELYSIAN_BDAY_DB_LINK: str = os.getenv("ELYSIAN_BDAY_DB_LINK", "")
--- birthday_reminder/main.py
+++ birthday_reminder/main.py
@@ -1,2 +1,17 @@
+from telegram import Bot, Update
+from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
+
+from birthday_reminder import BOT_TOKEN
+
+bot: Bot = Bot(BOT_TOKEN)
+app: Application = ApplicationBuilder().token(BOT_TOKEN).bot(bot).build()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    assert update.message is not None
+    await update.message.reply_text("hi!")
+
+
 def main() -> None:
-    pass
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling(drop_pending_updates=True)
