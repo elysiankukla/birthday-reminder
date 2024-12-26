30a23e1a1e85f710a2965b868bc2e1f210f3ff28
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-12-27 00:00:10 +0800

    unfinished implementation 2

--- birthday_reminder/main.py
+++ birthday_reminder/main.py
@@ -1,7 +1,11 @@
 from telegram import Bot, User
 from telegram.ext import ApplicationBuilder, Application, ContextTypes
 
-app: Application = ApplicationBuilder().token()
+from birthday_reminder import BOT_TOKEN
+
+bot: Bot = Bot(BOT_TOKEN)
+app: Application = ApplicationBuilder().token(BOT_TOKEN).bot(bot).build()
+
 
 def main() -> None:
     pass
