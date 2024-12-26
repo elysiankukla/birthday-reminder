45649754da8800ae0847a2c267cc047a3091527c
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-12-27 00:07:26 +0800

    finished intermediate

--- birthday_reminder/main.py
+++ birthday_reminder/main.py
@@ -1,5 +1,5 @@
-from telegram import Bot, User
-from telegram.ext import ApplicationBuilder, Application, ContextTypes
+from telegram import Bot, Update
+from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
 
 from birthday_reminder import BOT_TOKEN
 
@@ -7,5 +7,11 @@ bot: Bot = Bot(BOT_TOKEN)
 app: Application = ApplicationBuilder().token(BOT_TOKEN).bot(bot).build()
 
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    assert update.message is not None
+    await update.message.reply_text("hi!")
+
+
 def main() -> None:
-    pass
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling(drop_pending_updates=True)
