6d10b8260fd2307318170e96108cd7716e5b30a5
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2025-01-12 00:36:46 +0800

    initial implementation of scheduled wish

--- birthday_reminder/__init__.py
+++ birthday_reminder/__init__.py
@@ -2,5 +2,7 @@ import os
 
 import birthday_reminder.logging  # type: ignore (not meant to be accessed, stuffs are done in the file)
 
+TZ: str = "Asia/Kuala_Lumpur"
 BOT_TOKEN: str = os.getenv("ELYSIAN_BDAY_BOT_TOKEN", "")
 ELYSIAN_BDAY_DB_LINK: str = os.getenv("ELYSIAN_BDAY_DB_LINK", "")
+GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
--- birthday_reminder/main.py
+++ birthday_reminder/main.py
@@ -1,10 +1,38 @@
-from telegram import Bot, Update
-from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
+import datetime
+import logging
+import os
+from zoneinfo import ZoneInfo
 
-from birthday_reminder import BOT_TOKEN
+import google.generativeai as genai
+from telegram import Update
+from telegram.ext import (
+    Application,
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    Defaults,
+)
+from telegram.helpers import escape_markdown
 
-bot: Bot = Bot(BOT_TOKEN)
-app: Application = ApplicationBuilder().bot(bot).build()
+from birthday_reminder import BOT_TOKEN, TZ
+from birthday_reminder.database import Database
+
+defaults: Defaults = Defaults(tzinfo=ZoneInfo(TZ))
+log: logging.Logger = logging.getLogger(__name__)
+app: Application = ApplicationBuilder().token(BOT_TOKEN).defaults(defaults).build()
+db: Database = Database()
+
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY", ""))
+
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 40,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(model_name="gemini-2.0-flash-exp", generation_config=generation_config)  # type: ignore
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -12,6 +40,30 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("hi!")
 
 
+async def daily_trigger(context: ContextTypes.DEFAULT_TYPE) -> None:
+    db.prepare_database()
+    dt = datetime.datetime.now(ZoneInfo(TZ))
+    bday: list[str] = db.get_birthday_for_day(dt.month, dt.day)
+    if len(bday) == 0:
+        return
+
+    chat_session = model.start_chat(history=[])
+    response = await chat_session.send_message_async("Generate a short birthday wish.")
+    text = (
+        "**"
+        + escape_markdown("Today is the birthday of:")
+        + "**\n"
+        + escape_markdown(", ".join(bday))
+        + escape_markdown(f"\n{response.text}")
+    )
+
+    await context.bot.send_message(-1001264770246, text)
+
+
 def main() -> None:
+    log.info("setting up handlers...")
+    assert app.job_queue is not None
+    app.job_queue.run_daily(daily_trigger, datetime.time(hour=0, minute=0, second=0, tzinfo=ZoneInfo(TZ)))
     app.add_handler(CommandHandler("start", start))
-    app.run_polling(drop_pending_updates=True)
+    log.info("done")
+    app.run_polling(drop_pending_updates=True, poll_interval=60)
